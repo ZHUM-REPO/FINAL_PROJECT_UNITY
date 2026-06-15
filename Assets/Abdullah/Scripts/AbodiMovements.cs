4a7fd6e71d8435955d0a85790ec8762bad9aebf4
Abdullah <AbdullahAljamaan37@gmail.com>
2026-06-15 22:22:32 +0300

    Player inputs script and movements script
    it needs to be modified

---
+++ Assets/Abdullah/Scripts/AbodiMovements.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class AbodiMovements : MonoBehaviour
+{
+    [SerializeField] private CharacterController myCharacter;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private PlayerInputs playerInputs;
+    private Vector3 move;
+    private Vector3 velocity;
+
+
+    private void OnEnable()
+    {
+        PlayerInputs.OnMoveInput += HandleMoveInput;
+    }
+
+    private void OnDisable()
+    {
+        PlayerInputs.OnMoveInput -= HandleMoveInput;
+    }
+    void Start()
+    {
+        myCharacter = GetComponent<CharacterController>();
+    }
+
+
+    private void HandleMoveInput(Vector2 moveInput)
+    {
+        move = moveInput.x * transform.right + moveInput.y * transform.forward;
+        myCharacter.Move(move * moveSpeed * Time.deltaTime);
+    }
+
+    private void GravityLogic()
+    {
+        velocity.y += gravity * Time.deltaTime;
+        if (myCharacter.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        myCharacter.Move(velocity * Time.deltaTime);
+
+    }
+    
+
+    private void Update()
+    {   
+        HandleMoveInput(playerInputs.inputMove); // Ensure movement is processed every frame
+        GravityLogic();
+    }
+}
--- Assets/Abdullah/Scripts/PlayerInputs.cs
+++ Assets/Abdullah/Scripts/PlayerInputs.cs
@@ -1,16 +1,62 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInputs : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static event Action<Vector2> OnMoveInput;
+    public static event Action<Vector2> OnLookInput;
+    public static event Action OnJumpInput;
+    public static event Action OnCrouchInput;
+    public static event Action OnSprintInput;
+    public static event Action OnPauseInput;
+
+    public Vector2 inputMove;
+
+
+    public void MoveInput(InputAction.CallbackContext context)
     {
-        
+        inputMove = context.ReadValue<Vector2>();
+        OnMoveInput?.Invoke(inputMove);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LookInput(InputAction.CallbackContext context)
     {
-        
+        if (context.performed)
+        {
+            OnLookInput?.Invoke(context.ReadValue<Vector2>());
+        }
+    }
+
+    public void JumpInput(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            OnJumpInput?.Invoke();
+        }
+    }
+    
+    public void CrouchInput(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            OnCrouchInput?.Invoke();
+        }
+    }
+
+    public void SprintInput(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            OnSprintInput?.Invoke();
+        }
+    }
+
+    public void PauseInput(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            OnPauseInput?.Invoke();
+        }
     }
 }
